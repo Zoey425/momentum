63207d67297ca41333da38a3db7bb522eb8fdaa0
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-03-23 16:23:10 +0900

    click event changing color

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
 // title.innerText = "I got you!"
 // innerText
 // innerHTML
@@ -10,83 +10,104 @@ const title = document.getElementById("title");
 
 // elements 를 한번에 셀렉할수 있는 방법들 중 2가지
 
-const hellos = document.getElementsByClassName("hello");
-//  use getElementsByClassName when you want select many elements at the same time.
-console.log(hellos);
+// const hellos = document.getElementsByClassName("hello");
+// //  use getElementsByClassName when you want select many elements at the same time.
+// console.log(hellos);
 
 
-const titleV = document.getElementsByTagName("h1");
-console.log(titleV);
+// const titleV = document.getElementsByTagName("h1");
+// console.log(titleV);
 
 
 // What is the querySelector?
 // querySelector는 element를 css방식으로 검색 가능
  
 //querySelector 오직 하나의 첫번째 element만 return해준다.
-const queryS = document.querySelector(".bye h1");
-console.log(queryS);
+// const queryS = document.querySelector(".bye h1");
+// console.log(queryS);
 
 
-const querySAll = document.querySelectorAll(".bye h1");
+// const querySAll = document.querySelectorAll(".bye h1");
 // selector 안의 조건에 부합하는 모든 element를 가져다 준다.
 
-console.log(querySAll);
+// console.log(querySAll);
 
-const test = document.querySelector("#title");
-const test1 = document.getElementById("title");
+// const test = document.querySelector("#title");
+// const test1 = document.getElementById("title");
 // 위의 두 문장은 같은걸 불러온다 
 // 하지만 querySelector는 #title h1가능하지만 getElementById는 불가능.
 
-title.innerHTML = "Hello";
-title.style.color = "blue";
+// title.innerHTML = "Hello";
+// title.style.color = "blue";
 
 
 
 
 // Click Event!
-const hOne = document.querySelector("div.bye:first-child h1");
-console.log(hOne)
+// const hOne = document.querySelector("div.bye:first-child h1");
+// console.log(hOne)
 
 
-function handleTitleClick() {
-  hOne.style.color = "green";
-}
+// function handleTitleClick() {
+//   hOne.style.color = "green";
+// }
 
-function handleMouseEnter() {
-  hOne.innerText = "Mouse is here";
-}
+// function handleMouseEnter() {
+//   hOne.innerText = "Mouse is here";
+// }
 
-function handleMouseLeave() {
-  hOne.innerText = "Mouse is gone!"
-}
+// function handleMouseLeave() {
+//   hOne.innerText = "Mouse is gone!"
+// }
 
-function handleWindowResize(){
-  document.body.style.backgroundColor = "yellow";
-}
+// function handleWindowResize(){
+//   document.body.style.backgroundColor = "yellow";
+// }
 
-function handWindowCopy() {
-  alert("copier!")
-}
+// function handWindowCopy() {
+//   alert("copier!")
+// }
 
 
-function handleWindowOffline() {
-  alert("NO WIFI CONNECTION")
-}
+// function handleWindowOffline() {
+//   alert("NO WIFI CONNECTION")
+// }
 
-function handleWindowOnline() {
-  alert("ALL GOOD")  
-}
+// function handleWindowOnline() {
+//   alert("ALL GOOD")  
+// }
 
 // hOne.addEventListener("click" , handleTitleClick);
 // 위의 줄 코드를 onclick으로 변경 가능
-hOne.onClick = handleTitleClick;
-hOne.addEventListener("mouseenter", handleMouseEnter);
-hOne.addEventListener("mouseleave", handleMouseLeave);
+// hOne.onClick = handleTitleClick;
+// hOne.addEventListener("mouseenter", handleMouseEnter);
+// hOne.addEventListener("mouseleave", handleMouseLeave);
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handWindowCopy);
+// window.addEventListener("resize", handleWindowResize);
+// window.addEventListener("copy", handWindowCopy);
 
 
-// wifi
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+// // wifi
+// window.addEventListener("offline", handleWindowOffline);
+// window.addEventListener("online", handleWindowOnline);
+
+
+
+
+// Click Event with IF
+const h1 = document.querySelector("div.nice:first-child h1");
+
+
+
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if(currentColor === 'blue') {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
+}
+
+h1.addEventListener("click", handleTitleClick)
