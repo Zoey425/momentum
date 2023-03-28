975c0da4be8e07c2bb4f0e4a6a358fe1144087d2
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-03-28 17:09:47 +0900

    username 유무로 맞는 페이지 show

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 
 function onSubmit(event){
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   // saving username
-  localStorage.setItem("username", username)
+  localStorage.setItem(USERNAME_KEY, username)
 
-  greeting.innerText = `Hello ${username}`;
-  // = greeting.innerText = "Hello " + username;
-
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
 // recap!
@@ -25,11 +24,22 @@ function onSubmit(event){
 // greeting.classList.remove(HIDDEN_CLASSNAME) : 그 다음에 hidden이라는 클래스 이름을 remove 시켜준다.
 
 
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-function handleLinkClick(tomato) {
-  tomato.preventDefault();
-  console.log(tomato);
-} 
-// handleLinkClick({information about the event that just happened})
 
-loginForm.addEventListener("submit", onSubmit);
+// local storage에 username 값의 유무확인
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+console.log(savedUsername);
+
+
+if(savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onSubmit);
+} else {
+  // show the greetings
+  paintGreetings(username);
+}
