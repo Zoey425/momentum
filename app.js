86b62a94bd87d122fff7818a43f5beae437cd23d
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-03-28 16:19:12 +0900

    getting Username commit

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const loginForm = document.getElementById("login-form");
-//const loginForm = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const HIDDEN_CLASSNAME = "hidden";
 
-
-function handleBtnClick(){
-  const value = loginInput.value;
-  if (value === "") {
-    console.log("please write your name");
-  } else if(value.length > 15){
-    alert("Your name is too long");
-  } else{
-    console.log(value)
-  }
+function onSubmit(event){
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  //greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", handleBtnClick);
+
+function handleLinkClick(tomato) {
+  tomato.preventDefault();
+  console.log(tomato);
+} 
+// handleLinkClick({information about the event that just happened})
+
+loginForm.addEventListener("submit", onSubmit);
