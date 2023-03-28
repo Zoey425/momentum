4140d7417263137faa763f1aeb7892e04611723f
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-03-28 16:31:08 +0900

    getting username recap

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ function onSubmit(event){
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+// recap!
+// event.preventDefault();  : 브라우저의 기본 동작을 막음
+// loginForm.classList.add(HIDDEN_CLASSNAME); : loginForm에 hidden 클래스를 추가
+// const username = loginInput.value; : loginInput의 값을 변수 username에 저장
+// greeting.innerText = `Hello ${username}` : 비어있는 h1인 greeting에 Hello ${username}라는 string을 넣어줌
+// greeting.classList.remove(HIDDEN_CLASSNAME) : 그 다음에 hidden이라는 클래스 이름을 remove 시켜준다.
+
+
 
 function handleLinkClick(tomato) {
   tomato.preventDefault();
