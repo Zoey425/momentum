ade490721d972cf611a86fa0665c41a813507ef0
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-03-29 16:41:47 +0900

    recap

--- js/background.js
+++ js/background.js
@@ -7,3 +7,10 @@ bgImage.src = `img/${chosenImage}`;
 console.log(bgImage);
 
 document.body.appendChild(bgImage);
+
+
+// recap
+// random() 함수의 숫자는 0과 1사이의 숫자다 (0.xxxxxxx)
+// 어떤 수 사이의 어떠한 랜덤한 수를 가져오고 싶을 때 해야할 일은
+// 그냥 그 수를 곱한다. 그게 어떠한 숫자든 상관 없이.
+// floor()로 반올림해준다.
