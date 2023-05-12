add7132fc98b7807150d90291646244e55df27af
Eunyoung Park <108717962+Zoey425@users.noreply.github.com>
2023-05-12 14:55:36 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"];
+const images = ["01.JPG", "02.JPG", "03.JPG", "04.JPG"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.querySelector("body");
